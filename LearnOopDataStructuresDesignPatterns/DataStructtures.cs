ea0fd77c29bece266d7d5c0bb3c353ec7334fa4f
tahir-aslam <tahir_aslam786@yahoo.com>
2024-11-05 19:22:40 +0500

    datastructres

--- LearnOopDataStructuresDesignPatterns/DataStructtures.cs
+++ LearnOopDataStructuresDesignPatterns/DataStructtures.cs
@@ -23,6 +23,7 @@ namespace LearnOopDataStructuresDesignPatterns
         // Insert a new node at the end
         public void Add(int data)
         {
+            //O(n)
             Node newNode = new Node(data);
 
             if (head == null)
@@ -40,9 +41,21 @@ namespace LearnOopDataStructuresDesignPatterns
             }
         }
 
+        //Insert a new node at the beggining
+        public void Insert(int data)
+        {
+            //0(1)
+            Node newNode = new Node(data);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        //Remove node
+
         // Print all nodes
         public void Print()
         {
+            //(O(n))
             Console.WriteLine("Linked List");
             Node temp = head;
             while (temp != null)
@@ -64,4 +77,458 @@ namespace LearnOopDataStructuresDesignPatterns
             this.Next = null;
         }
     }
+
+    //Stacks
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedListStack<T>
+    {
+        private Node<T> top;
+
+        public LinkedListStack()
+        {
+            top = null;
+        }
+
+        // Push operation - Adds an element to the top of the stack
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = top;  // Link the new node to the previous top
+            top = newNode;  // Make the new node the new top
+            Console.WriteLine($"{data} pushed onto the stack.");
+        }
+
+        // Pop operation - Removes and returns the top element
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty. Cannot perform pop operation.");
+            }
+
+            T value = top.Data;
+            top = top.Next;  // Move top to the next node
+            Console.WriteLine($"{value} popped from the stack.");
+            return value;
+        }
+
+        // Peek operation - Returns the top element without removing it
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return top.Data;
+        }
+
+        // IsEmpty operation - Checks if the stack is empty
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+        // Print the stack contents
+        public void PrintStack()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty.");
+                return;
+            }
+
+            Node<T> currentNode = top;
+            Console.WriteLine("Stack contents (from top to bottom):");
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Data);
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+
+    //Queue
+    public class CustomQueue<T>
+    {
+        private Node<T> head; // Front of the queue
+        private Node<T> tail; // Rear of the queue
+        private int size;     // Number of elements in the queue
+
+        // Constructor
+        public CustomQueue()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        // Enqueue: Add an element to the rear of the queue
+        public void Enqueue(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+
+            if (tail == null) // If the queue is empty
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode; // Link the old tail to the new node
+                tail = newNode;      // Update the tail
+            }
+
+            size++;
+        }
+
+        // Dequeue: Remove and return the front element from the queue
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            T frontValue = head.Data;
+            head = head.Next; // Move the head to the next node
+
+            if (head == null) // If the queue is now empty
+            {
+                tail = null; // Reset the tail as well
+            }
+
+            size--;
+            return frontValue;
+        }
+
+        // Peek: Return the front element without removing it
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return head.Data;
+        }
+
+        // IsEmpty: Check if the queue is empty
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        // Size: Get the number of elements in the queue
+        public int Size()
+        {
+            return size;
+        }
+
+        // Display the queue (for testing purposes)
+        public void DisplayQueue()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    //HashMap
+    public class CustomHashTable
+    {
+        public class Node
+        {
+            public string Key { get; set; }
+            public int Value { get; set; }
+            public Node Next { get; set; }
+
+            public Node(string key, int value)
+            {
+                Key = key;
+                Value = value;
+                Next = null;
+            }
+        }
+
+        private readonly int size;
+        private readonly Node[] buckets;
+
+        public CustomHashTable(int size)
+        {
+            this.size = size;
+            buckets = new Node[size];  // Initialize the buckets (array of linked lists)
+        }
+
+        // Hash function to convert key to index
+        private int GetBucketIndex(string key)
+        {
+            int hashCode = key.GetHashCode();    // Get the hash code of the string key
+            int index = Math.Abs(hashCode % size);  // Use modulus to get an index within the array size
+            return index;
+        }
+
+        // Insert or update a key-value pair in the hash table
+        public void Insert(string key, int value)
+        {
+            int index = GetBucketIndex(key);   // Get the bucket index for the key
+            Node head = buckets[index];        // Get the head of the linked list at that index
+
+            // Check if the key already exists, if so, update the value
+            while (head != null)
+            {
+                if (head.Key.Equals(key))
+                {
+                    head.Value = value;  // Update the existing value
+                    return;
+                }
+                head = head.Next;  // Move to the next node in the linked list
+            }
+
+            // Key does not exist, insert new node at the head of the linked list
+            Node newNode = new Node(key, value);
+            newNode.Next = buckets[index];  // Point the new node to the current head
+            buckets[index] = newNode;       // Make the new node the new head
+        }
+
+        // Retrieve the value associated with the given key
+        public int Get(string key)
+        {
+            int index = GetBucketIndex(key);   // Get the bucket index for the key
+            Node head = buckets[index];        // Get the head of the linked list at that index
+
+            // Search for the key in the linked list
+            while (head != null)
+            {
+                if (head.Key.Equals(key))
+                {
+                    return head.Value;  // Return the associated value99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999++-9
+                }
+                head = head.Next;  // Move to the next node in the list
+            }
+
+            throw new Exception("Key not found");  // If key not found, throw an exception
+        }
+
+        // Remove the key-value pair for the given key
+        public void Remove(string key)
+        {
+            int index = GetBucketIndex(key);   // Get the bucket index for the key
+            Node head = buckets[index];        // Get the head of the linked list at that index
+            Node previous = null;
+
+            // Search for the key in the linked list
+            while (head != null)
+            {
+                if (head.Key.Equals(key))
+                {
+                    if (previous == null)
+                    {
+                        // Key is at the head, so update the head to the next node
+                        buckets[index] = head.Next;
+                    }
+                    else
+                    {
+                        // Bypass the node to remove it
+                        previous.Next = head.Next;
+                    }
+                    return;
+                }
+
+
+                previous = head;
+
+                throw new Exception("Key not found");
+            }
+        }
+
+    }
+
+    //Binary Tree
+    public class BinaryTree
+    {
+        public class TreeNode
+        {
+            public int Value;
+            public TreeNode Left;
+            public TreeNode Right;
+
+            public TreeNode(int value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public TreeNode Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        // Insert a node into the binary tree
+        public void Insert(int value)
+        {
+            Root = InsertRecursively(Root, value);
+        }
+
+        private TreeNode InsertRecursively(TreeNode node, int value)
+        {
+            if (node == null)
+            {
+                return new TreeNode(value);
+            }
+
+            if (value < node.Value)
+            {
+                node.Left = InsertRecursively(node.Left, value);
+            }
+            else if (value > node.Value)
+            {
+                node.Right = InsertRecursively(node.Right, value);
+            }
+
+            return node;
+        }
+
+        // In-Order Traversal: Left -> Root -> Right
+        public void InOrderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                InOrderTraversal(node.Left);
+                Console.Write(node.Value + " ");
+                InOrderTraversal(node.Right);
+            }
+        }
+
+        // Pre-Order Traversal: Root -> Left -> Right
+        public void PreOrderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                Console.Write(node.Value + " ");
+                PreOrderTraversal(node.Left);
+                PreOrderTraversal(node.Right);
+            }
+        }
+
+        // Post-Order Traversal: Left -> Right -> Root
+        public void PostOrderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                PostOrderTraversal(node.Left);
+                PostOrderTraversal(node.Right);
+                Console.Write(node.Value + " ");
+            }
+        }
+    }
+
+    //Binary Search Tree
+    public class BinarySearchTree
+    {
+        public class TreeNode
+        {
+            public int Value;
+            public TreeNode Left;
+            public TreeNode Right;
+
+            public TreeNode(int value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+
+        public TreeNode Root;
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        // Insert a node into the BST
+        public void Insert(int value)
+        {
+            Root = InsertRecursively(Root, value);
+        }
+
+        private TreeNode InsertRecursively(TreeNode node, int value)
+        {
+            if (node == null)
+            {
+                return new TreeNode(value);
+            }
+
+            if (value < node.Value)
+            {
+                node.Left = InsertRecursively(node.Left, value);
+            }
+            else if (value > node.Value)
+            {
+                node.Right = InsertRecursively(node.Right, value);
+            }
+
+            return node;
+        }
+
+        // Search for a value in the BST
+        public bool Search(int value)
+        {
+            return SearchRecursively(Root, value);
+        }
+
+        private bool SearchRecursively(TreeNode node, int value)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (value == node.Value)
+            {
+                return true;
+            }
+            else if (value < node.Value)
+            {
+                return SearchRecursively(node.Left, value);
+            }
+            else
+            {
+                return SearchRecursively(node.Right, value);
+            }
+        }
+
+        // In-Order Traversal: Left -> Root -> Right (Sorted order for BST)
+        public void InOrderTraversal(TreeNode node)
+        {
+            if (node != null)
+            {
+                InOrderTraversal(node.Left);
+                Console.Write(node.Value + " ");
+                InOrderTraversal(node.Right);
+            }
+        }
+    }
 }
+
+
--- LearnOopDataStructuresDesignPatterns/Program.cs
+++ LearnOopDataStructuresDesignPatterns/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LearnOopDataStructuresDesignPatterns;
+using System.Collections;
+using System.Collections.Concurrent;
 
 Console.WriteLine("Hello, World!");
 
@@ -22,5 +24,112 @@ linkedList.Add(1);
 linkedList.Add(2);
 linkedList.Add(3);
 linkedList.Print();
+
+// Stack
+LinkedListStack<int> stack = new LinkedListStack<int>();
+// Push elements onto the stack
+stack.Push(10);
+stack.Push(20);
+stack.Push(30);
+
+// Peek at the top element
+Console.WriteLine($"Top element is: {stack.Peek()}");
+
+// Pop elements from the stack
+stack.Pop();
+stack.Pop();
+
+// Check the top element again
+Console.WriteLine($"Top element after pops is: {stack.Peek()}");
+
+
+string firstName = "tahir";
+Console.WriteLine($" HashCode:  {firstName.GetHashCode()}");
+
+Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+
+//hashmap
+CustomHashTable hashTable = new CustomHashTable(3);
+hashTable.Insert("Alice", 25);
+hashTable.Insert("Bob", 30);
+hashTable.Insert("Charlie", 35);
+Console.WriteLine("Alice's age: " + hashTable.Get("Alice"));  // Output: 25
+Console.WriteLine("Bob's age: " + hashTable.Get("Bob"));      // Output: 30
+
+//Dictationary
+var dictionary = new Dictionary<string, int>();
+dictionary["apple"] = 1;
+int value = dictionary["apple"]; // Fast access using key
+
+//Hashset just unique values
+var hashSet = new HashSet<int>();
+hashSet.Add(1);
+hashSet.Add(2);
+bool contains = hashSet.Contains(2); // Fast lookup
+
+//ConcurrentDicationary Threadsafe
+var concurrentDict = new ConcurrentDictionary<string, int>();
+concurrentDict.TryAdd("key1", 100);
+concurrentDict.TryUpdate("key1", 200, 100);
+
+//Hashtable
+var hashtable = new Hashtable();
+hashtable["apple"] = 1;
+int valuehashtable = (int)hashtable["apple"]; // Requires casting
+
+//Binar Tree
+// Create a binary tree and insert nodes
+Console.WriteLine("\n---Binary Tree----:");
+BinaryTree tree = new BinaryTree();
+//tree.Insert(50);
+//tree.Insert(30);
+//tree.Insert(70);
+//tree.Insert(20);
+//tree.Insert(40);
+//tree.Insert(60);
+//tree.Insert(80);
+
+tree.Insert(1);
+tree.Insert(2);
+tree.Insert(3);
+tree.Insert(4);
+tree.Insert(5);
+tree.Insert(6);
+tree.Insert(7);
+
+Console.WriteLine("In-Order Traversal:");
+tree.InOrderTraversal(tree.Root);
+
+Console.WriteLine("\nPre-Order Traversal:");
+tree.PreOrderTraversal(tree.Root);
+
+Console.WriteLine("\nPost-Order Traversal:");
+tree.PostOrderTraversal(tree.Root);
+
+//50
+///    \
+//     30      70
+//    /  \    /  \
+//   20   40  60  80
+
+
+Console.WriteLine("\n\n---Binary Search Tree----:");
+// Create a binary search tree and insert nodes
+BinarySearchTree bst = new BinarySearchTree();
+bst.Insert(50);
+bst.Insert(30);
+bst.Insert(70);
+bst.Insert(20);
+bst.Insert(40);
+bst.Insert(60);
+bst.Insert(80);
+
+Console.WriteLine("In-Order Traversal:");
+bst.InOrderTraversal(bst.Root); // Outputs: 20 30 40 50 60 70 80
+
+// Search for a value in the BST
+Console.WriteLine("\nSearch for 40: " + bst.Search(40)); // Outputs: true
+Console.WriteLine("Search for 90: " + bst.Search(90)); // Outputs: false
+
 
 Console.ReadLine();
